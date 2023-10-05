225bb2427d2e2849d64fe405cb841117c36e628e
Madhan <madhan.p@unitedhands.cc>
2023-10-05 10:49:45 +0530

    Bank Payment create screen

--- src/app/Voucher/BankPayment/bank-payment-create/bank-payment-create.component.ts
+++ src/app/Voucher/BankPayment/bank-payment-create/bank-payment-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { dropDownData, exportOptions } from 'src/app/reports/stub/salesOrderStub';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-bank-payment-create',
@@ -7,9 +11,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BankPaymentCreateComponent implements OnInit {
 
-  constructor() { }
+  bankPaymentForm!: FormGroup;
+  vchdate: any;
+  saveAsOptions: dropDownData[] = exportOptions;
+  customerData: dropDownData[] = [];
+
+  paymentTypeData: dropDownData[] = [
+    { name: 'BILL BY BILL', id: 'BILL BY BILL' },
+    { name: 'ON ACCOUNT', id: 'ON ACCOUNT' },
+    { name: 'ADVANCE', id: 'ADVANCE' },
+  ];
+
+  paymentModeData: dropDownData[] = [
+    { name: 'CHEQUE', id: 'CHEQUE' },
+    { name: 'ONLINE', id: 'ONLINE' },
+  ];
+
+
+  constructor(
+    public api: ApiService,
+    private router: Router,
+    public fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.setValidations();
+    this.loadCustomers();
+    this.vchdate = new Date();
   }
 
+  setValidations() {
+    this.bankPaymentForm = this.fb.group({
+      cvoucherno: ['', [Validators.required]],
+      cvchdate: ['', [Validators.required]],
+      cpaymenttype: ['', [Validators.required]],
+      ccustomercode: ['', [Validators.required]],
+      caccountcode: ['', [Validators.required]],
+      cpaymentmode: ['', [Validators.required]],
+      cvoucheramount: ['', [Validators.required]],
+      cremarks: ['', [Validators.required]],
+    });
+  }
+
+  onClickButton(): void {
+    this.router.navigateByUrl('bank-payment-list');
+  }
+
+  clear() {
+    this.bankPaymentForm.reset();
+  }
+
+  loadCustomers() {
+    this.api.getCustomersOnly().subscribe((res) => {
+      this.customerData = res.length
+        ? res.map((item: any) => {
+          return { name: item.companyDisplayName, id: item.ledgerCode };
+        })
+        : [];
+    });
+  }
 }
--- src/app/Voucher/BankPayment/bank-payment-list/bank-payment-list.component.ts
+++ src/app/Voucher/BankPayment/bank-payment-list/bank-payment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bank-payment-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BankPaymentListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  onClickCreate() {
+    this.router.navigateByUrl('bank-payment-create');
   }
 
 }
